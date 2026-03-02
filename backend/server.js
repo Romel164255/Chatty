4542665fa178c59e2b2ce872b9aff5fe8d75b741
nightraven16 <romelaugustine3@gmail.com>
2026-03-02 12:35:36 +0530

    basic backend and frontend

---
+++ backend/server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: { origin: "*" }
+});
+
+io.on("connection", (socket) => {
+  console.log("User connected");
+
+  socket.on("join_room", (roomId) => {
+    socket.join(roomId);
+  });
+
+  socket.on("send_message", async (data) => {
+    // Save to DB later
+    io.to(data.roomId).emit("receive_message", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
+});
+
+server.listen(3000, () => {
+  console.log("Server running");
+});
